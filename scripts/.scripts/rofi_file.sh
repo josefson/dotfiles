0bea6ed672231d6689032b9ed3faade6c715abb2
Josefson Fraga <josefson@users.noreply.github.com>
2018-08-19 02:05:05 -0400

    deleting rofi scripts

--- scripts/.scripts/rofi_file.sh
+++
@@ -1,7 +0,0 @@
-!#/usr/bin/bash
-xdg-open "$(locate home | rofi -threads 0 \
-    -color-window '#000000, #000000, #000000' \
-    -color-normal '#000000, #ff4000, #000000, #ff4000, #000000' \
-    -color-active '#000000, #ff4000, #000000, #ff4000, #000000' \
-    -color-urgent '#000000, #ff4000, #000000, #ff4000, #000000' \
-    -dmenu -i -p 'locate:')"
--- scripts/.scripts/rofi_switcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-rofi -show window \
-    -color-window '#000000, #000000, #000000' \
-    -color-normal '#000000, #ff4000, #000000, #ff4000, #000000' \
-    -color-active '#000000, #ff4000, #000000, #ff4000, #000000' \
-    -color-urgent '#000000, #ff4000, #000000, #ff4000, #000000'
