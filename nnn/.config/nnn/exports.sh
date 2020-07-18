df838b512789af0e88cf25ec0d6067b168de4145
Josefson Fraga <josefson.br@gmail.com>
2020-07-18 16:33:17 -0400

    add nnn to dotfiles

---
+++ nnn/.config/nnn/exports.sh
@@ -0,0 +1,35 @@
+export NNN_OPTS='n'
+export NNN_FIFO='/tmp/nnn.fifo'
+export NNN_SEL='/tmp/.sel'
+export NNN_TRASH=1                                 # trash (needs trash-cli) instead of delete
+export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)$"
+export NNN_BMS=''\
+'b:~/bin;'\
+'c:~/.config;'\
+'d:~/Downloads;'\
+'i:~/.config/i3;'\
+'h:~/;'\
+'q:~/.config/qutebrowser;'\
+'t:~/DATA/Torrents;'\
+'m:~/Music;'\
+'p:~/Workspace/Python;'\
+'v:~/Videos;'\
+'w:~/Workspace;'\
+'R:/;'\
+'D:~/DATA;'\
+'E:/etc;'\
+'H:/home;'\
+'M:/media;'\
+'N:/mnt;'\
+'T:/tmp;'\
+'U:/usr;'\
+'V:/var;'
+export NNN_PLUG=''\
+'c:fzcd;'\
+'h:fzhist;'\
+'o:fzopen;'\
+'z:fzz;'\
+'u:getplugs;'\
+'O:launch;'\
+'s:suedit;'\
+'t:tree;'
